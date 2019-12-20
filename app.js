2e72cf14e77493dfdfdb9525ec813359ea1fa5ad
Tarun Gorai <goraitarunkumar@gmail.com>
2019-12-20 22:57:29 +0530

    perse request body

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-
+const fs = require('fs');
 // function reqListner(req, res){
 
 // }
@@ -9,7 +9,33 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
     // console.log(req)
-    console.log(req.url, req.method, req.headers);
+    // console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    if (url ===  '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>')
+        res.write('</html>');
+        return res.end();
+    }
+    if( url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log({chunk})
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const perseBody = Buffer.concat(body).toString();
+            console.log({perseBody})
+            const message = perseBody.split('=')[1];
+            fs.writeFileSync('message.txt', message)
+        });
+        
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('content-type','text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
