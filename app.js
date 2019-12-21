442c5c0721bc31c98bee753e3fcbc66db4dd02da
Tarun Gorai <goraitarunkumar@gmail.com>
2019-12-21 13:05:58 +0530

    add prefix and filtering paths

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
+
+//for 404 page
+
+app.use('/', (req, res, next) => {
+    res.status(404).send('<h1>Page Not Found!!</h1>');
+});
 
 app.listen(3000);
