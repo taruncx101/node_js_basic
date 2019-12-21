719414dac276e9486860a2a460202f8dc810cda5
Tarun Gorai <goraitarunkumar@gmail.com>
2019-12-21 11:48:12 +0530

    adding routes to express

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const http = require('http');
 
-
 const express = require('express');
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next(); // allows request to continue next middleware
+app.use('/',(req, res, next) => {
+    console.log('This middleware always run.');
+    next();
+});
+app.use('/add-product',(req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Add product page</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
     console.log('In another middleware');
     res.send('<h1>Hello from Express</h1>');
 });
-
-// const server = http.createServer(app);
-
-// server.listen(3000);
-
-// or
-
 app.listen(3000);
