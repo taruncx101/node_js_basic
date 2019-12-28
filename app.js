0f1d34074018f8ce5c4673fbcc87a6404c8890eb
Tarun Gorai <goraitarunkumar@gmail.com>
2019-12-28 13:06:10 +0530

    if  else and for loop in ejs template

--- app.js
+++ app.js
@@ -34,8 +34,7 @@ app.use(shopRoutes);
 app.use('/', (req, res, next) => {
     // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
     res.status(404).render('404', {
-        pageTitle: 'Page Not Found',
-        layout: false,
+        pageTitle: 'Page Not Found'
     });
 });
 
