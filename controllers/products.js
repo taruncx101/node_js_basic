5bce70059d2ee664cdb5e3ed89010aa77553e9ba
Tarun Gorai <goraitarunkumar@gmail.com>
2019-12-29 13:23:25 +0530

    creating structure of the app

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     // res.sendFile(path.join(rootDir,'views','add-product.html'));
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         activeAddProduct: true,
@@ -23,7 +23,7 @@ exports.getProducts = (req, res, next) => {
     // res.sendFile(path.join(rootDir, 'views','shop.html'));
     // const products = adminData.products;
     Product.fetchAllproducts((products)=>{
-        res.render('shop', {
+        res.render('shop/product-list', {
             prods: products,
             pageTitle: 'Shop',
             path: '/',
