d704a17637332526822dbdf9850b461e8726ed8e
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-05 08:14:37 +0530

    added orders section

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,9 @@ router.get('/',shopController.getIndex);
 
 router.get('/products',shopController.getProducts);
 
-router.get('/cart',shopController.getCart);
+router.get('/cart', shopController.getCart);
+
+router.get('/orders',shopController.getOrders);
 
 router.get('/checkout',shopController.getCheckout);
 
