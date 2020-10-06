9037cf4e5abc9506ffe302253583ec57f29d0215
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-06 08:30:04 +0530

    Added dynamic route

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,16 @@ exports.getProducts = (req, res, next) => {
     
 }
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.fetchAllproducts((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  });
+};
 exports.getIndex = (req, res, next) => {
     Product.fetchAllproducts((products)=>{
         res.render('shop/index', {
