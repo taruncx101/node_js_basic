0e3d932de84c12fb6794dd8cd7971e0412fa21d1
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-07 11:51:09 +0530

    Added db conncetion and execute a query

--- app.js
+++ app.js
@@ -6,16 +6,28 @@ const bodyParser = require('body-parser');
 
 const rootDir = require('./utils/path');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 
-const errorController = require('./controllers/error')
+const db = require("./utils/database");
+
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const errorController = require("./controllers/error");
 
 const app = express();
 
+db.execute('SELECT * FROM products')
+    .then((result) => {
+        console.log(result)
+    })
+    .catch((err) => {
+        console.log(err)
+    });
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -29,4 +41,4 @@ app.use(shopRoutes);
 
 app.use('/',errorController.get404);
 
-app.listen(3000);
+app.listen(3001);
