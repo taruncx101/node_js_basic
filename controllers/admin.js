4a72bb2d44ad6595b93c707a014f9d55751a7bdb
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-08 15:46:31 +0530

    implemented product delete functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -24,7 +24,7 @@ exports.postAddProduct = (req, res, next) => {
     })
       .then((result) => {
         console.log(result);
-         res.redirect("/");
+         res.redirect("/admin/products");
       })
       .catch((err) => {
         console.log(err);
@@ -79,8 +79,16 @@ exports.postEditProduct = (req, res, next) => {
 }
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+           Product.findByPk(prodId)
+             .then((product) => {
+               return product.destroy();
+             })
+             .then((result) => {
+               console.log("destroyed product");
+               res.redirect("/admin/products");
+             })
+             .catch((err) => console.log(err));
+  
 };
 exports.getProducts = (req, res, next) => {
       Product.findAll()
