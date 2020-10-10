569c7d72180acd3a8edb3377e9db8843a334e106
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-10 22:47:07 +0530

    Implemented delete product functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,5 @@
 const mongodb = require('mongodb')
 const Product = require('../models/product');
-const ObjectId = mongodb.ObjectId;
 exports.getAddProduct = (req, res, next) => {
     // res.sendFile(path.join(rootDir,'views','add-product.html'));
     res.render('admin/edit-product', {
@@ -46,7 +45,7 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
 
-      Product.findById(new ObjectId(prodId))
+      Product.findById(prodId)
         .then((product) => {
           if (!product) {
             return res.redirect("/");
@@ -69,13 +68,7 @@ exports.postEditProduct = (req, res, next) => {
       const price = req.body.price;
       const description = req.body.description;
 
-      const product = new Product(
-        title,
-        price,
-        description,
-        imageUrl,
-        new ObjectId(prodId)
-      );
+      const product = new Product(title, price, description, imageUrl, prodId);
         product.save()
       .then((result) => {
         console.log("product updated");
@@ -88,10 +81,7 @@ exports.postEditProduct = (req, res, next) => {
 }
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-           Product.findByPk(prodId)
-             .then((product) => {
-               return product.destroy();
-             })
+           Product.deleteById(prodId)
              .then((result) => {
                console.log("destroyed product");
                res.redirect("/admin/products");
--- models/product.js
+++ models/product.js
@@ -6,20 +6,21 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = id;
+    this._id = new mongodb.ObjectId(id);
   }
   save() {
     const db = getDb();
     let dbOp;
     if (this._id) {
       //update the product
-      dbOp = db.collection("products").updateOne({_id: this._id}, {$set: this})
-    }
-    else {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
       //insert
       dbOp = db.collection("products").insertOne(this);
     }
-   
+
     return dbOp
       .then((result) => {
         console.log(result);
@@ -38,15 +39,25 @@ class Product {
       .catch((err) => console.log(err));
   }
   static findById(prodId) {
-        const db = getDb();
-        return db
-          .collection("products")
-          .find({ _id: prodId})
-          .next()
-          .then((product) => {
-            return product;
-          })
-          .catch((err) => console.log(err));
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        return product;
+      })
+      .catch((err) => console.log(err));
+  }
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        return result;
+      })
+      .catch((err) => console.log(err));
   }
 }
 
