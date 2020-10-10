c39b068e5b2e36ba5c8ab9a1a2ba84654fd27718
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-10 16:47:59 +0530

    Adding Order and OrderItem model

---
+++ models/order.js
@@ -0,0 +1,16 @@
+const db = require("../utils/database");
+
+const Sequelize = require("sequelize");
+
+const sequelize = require("../utils/database");
+
+const Order = sequelize.define("order", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+});
+
+module.exports = Order;
