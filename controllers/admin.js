e04d48825deec4ce22cfd0d07f58eb98dea6410c
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-11 17:03:25 +0530

    adding relation via populate

--- controllers/admin.js
+++ controllers/admin.js
@@ -89,6 +89,8 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 exports.getProducts = (req, res, next) => {
   Product.find()
+    //.select('title price -_id')
+    //.populate('userId', 'name')
     .then((products) => {
       res.render("admin/products", {
         prods: products,
