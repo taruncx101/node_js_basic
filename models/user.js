e4077dc07d29c19a1ac105caecffda8ce3b47781
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-11 14:46:09 +0530

    displaying the orders

--- models/user.js
+++ models/user.js
@@ -108,7 +108,9 @@ class User {
     const db = getDb();
     return db
       .collection("orders")
-      .find()
+      .find({
+        'user._id': new ObjectId(this._id)
+      })
       .toArray()
       .then((orders) => {
         return orders;
