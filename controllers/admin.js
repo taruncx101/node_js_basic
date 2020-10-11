479a33aa9dda21bf5bd4b067e1c2ef1bbbbd236f
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-11 16:11:57 +0530

    Delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -78,7 +78,7 @@ exports.postEditProduct = (req, res, next) => {
 }
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-           Product.deleteById(prodId)
+           Product.findByIdAndRemove(prodId)
              .then((result) => {
                console.log("destroyed product");
                res.redirect("/admin/products");
--- routes/admin.js
+++ routes/admin.js
@@ -20,6 +20,6 @@ router.post("/edit-product", adminController.postEditProduct);
 // // =>post requests
 router.post('/add-product', adminController.postAddProduct);
 
-// router.post("/delete-product", adminController.postDeleteProduct);
+router.post("/delete-product", adminController.postDeleteProduct);
 
 module.exports= router;
