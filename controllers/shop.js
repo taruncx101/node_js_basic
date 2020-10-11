dc896d42a9b559436f1f33edce6765c33dc86251
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-11 17:19:00 +0530

    getting cart data

--- controllers/shop.js
+++ controllers/shop.js
@@ -49,9 +49,10 @@ exports.getIndex = (req, res, next) => {
 }
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
-      console.log(products);
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;;
         res.render("shop/cart", {
           pageTitle: "Your Cart",
           path: "/cart",
