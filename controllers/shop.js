b6c7105b0c2b14eb877b5e23bea04f5814ec9df7
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-12 07:33:00 +0530

    Displaying the orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -114,8 +114,9 @@ exports.postOrder = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user
-  .getOrders()
+  Order.find({
+    "user.userId": req.user._id
+  })
   .then((orders) => {
       res.render("shop/orders", {
         pageTitle: "Your Orders",
