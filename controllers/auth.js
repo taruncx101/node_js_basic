ed894ea691e1058cb65f1950feb05e84e6f7a3a1
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-13 06:30:38 +0530

    setting the signup page

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,39 @@
-const User = require("../models/user");
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  res.render("auth/login", {
-    pageTitle: "Log In",
-    path: "/login",
-    isAuthenticated: false,
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
   });
 };
-exports.postLogin = (req, res, next) => {
-      User.findById("5f82e96b9986183cd0d4496c")
-        .then((user) => {
-            req.session.user = user;
-          req.session.isLoggedIn = true;
-          req.session.save((err) => {
-            console.log(err);
-            res.redirect("/");
-          })
-        })
-        .catch((err) => console.log(err));
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
 };
-exports.postLogout = (req, res, next) => {
-    req.session.destroy(() => {
-              res.redirect("/");
-    })
 
+exports.postLogin = (req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
