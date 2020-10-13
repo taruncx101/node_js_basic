2ff5c576e4c6188c5214d0044a02f9c3384b683f
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-13 07:17:10 +0530

    encrypt the password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -38,20 +39,25 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
           return res.redirect('/signup')
       }
-      const user = new User({
-        email,
-        password,
-        cart: {items: []}
-      })
-      return user.save();
-    })
-    .then(user => {
-        req.session.isLoggedIn = true;
-        req.session.user = user;
-        req.session.save((err) => {
-          console.log(err);
-          res.redirect("/");
+     bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((user) => {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          req.session.save((err) => {
+            console.log(err);
+            res.redirect("/");
+          });
         });
+
     })
     .catch((err) => console.log(err));
 };
