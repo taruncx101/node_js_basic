421a40060b69fa1cbfc84413da8fcf3aa33e9e21
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-15 11:42:12 +0530

    creating pdf on the fly

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,8 @@ const { request } = require("express");
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const PDFDocument = require("pdfkit");
+
 exports.getProducts = (req, res, next) => {
     Product.find()
       .then((products) => {
@@ -167,20 +169,40 @@ exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
   const invoiceName = `invoice-${orderId}.pdf`;
   const invoicePath = path.join("data", "invoices", invoiceName);
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return next(new Error("No order found."));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error("Unauthorized."));
+      }
+      const pdfDoc = new PDFDocument();
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        'inline; filename="' + invoiceName + '"'
+      );
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+      pdfDoc.text("Hello world!");
+      pdfDoc.end();
+      // fs.readFile(invoicePath, (err, data) => {
+      //   if (err) {
+      //     return next(err)
+      //   }
+      //   res.setHeader('Content-Type', 'application/pdf');
+      //   //res.setHeader('Content-Disposition', 'attachment; filename="'+ invoiceName + '"');
+      //   res.setHeader('Content-Disposition', 'inline; filename="'+ invoiceName + '"');
+      //   res.send(data);
+      // });
+      // const file = fs.createReadStream(invoicePath);
+      // file.pipe(res);
+    })
+    .catch((err) => {
+      next(err);
+    });
 
-  // fs.readFile(invoicePath, (err, data) => {
-  //   if (err) {
-  //     return next(err)
-  //   }
-  //   res.setHeader('Content-Type', 'application/pdf');
-  //   //res.setHeader('Content-Disposition', 'attachment; filename="'+ invoiceName + '"');
-  //   res.setHeader('Content-Disposition', 'inline; filename="'+ invoiceName + '"');
-  //   res.send(data);
-  // });
-  const file = fs.createReadStream(invoicePath);
-  res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
-  file.pipe(res);
 };
 
 exports.getCheckout = (req, res, next) => {
