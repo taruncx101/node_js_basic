c31c85b7822b7a7fab0f56723f2ec7cb517e533f
Tarun Gorai <goraitarunkumar@gmail.com>
2020-10-15 13:16:37 +0530

    aadding dynamic pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,8 @@ const { request } = require("express");
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 1;
+
 const PDFDocument = require("pdfkit");
 
 exports.getProducts = (req, res, next) => {
@@ -43,12 +45,27 @@ exports.getProduct = (req, res, next) => {
 
 };
 exports.getIndex = (req, res, next) => {
-  Product.find()
+  const page = req.query.page || 1; 
+  let totalItems;
+  Product.countDocuments()
+    .then(count => {
+      totalItems = count;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
         path: "/",
+        totalproducts: totalItems,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        currentPage: page,
+        nextPage: +page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch((err) => {
